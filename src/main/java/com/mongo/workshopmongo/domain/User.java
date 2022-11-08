6b289d5752b2333f0a568b78e410944434c41e23
AleehSophia <alessia.casado@gmail.com>
2022-11-08 18:25:35 -0300

    feat: User

---
+++ src/main/java/com/mongo/workshopmongo/domain/User.java
@@ -0,0 +1,56 @@
+package com.mongo.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {}
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/mongo/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.mongo.workshopmongo.resources;
+
+import com.mongo.workshopmongo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(maria, alex));
+        return ResponseEntity.ok().body(list);
+    }
+}
