754533db0a8f8fb27759c0a4f2105b3247c7b7eb
AleehSophia <alessia.casado@gmail.com>
2022-11-09 11:47:42 -0300

    feat: database instantiation

---
+++ src/main/java/com/mongo/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.mongo.workshopmongo.config;
+
+import com.mongo.workshopmongo.domain.User;
+import com.mongo.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
